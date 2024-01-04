6f5fd60a7c03d4a65524f3b9124ccd9373fc69e5
Said elfazni <elfazni@gmail.com>
2024-01-04 10:58:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-resource "random_pet" "server_output" {
+resource "random_string" "server_output" {
   length           = 16
   special          = true
   override_special = "/@£$"
